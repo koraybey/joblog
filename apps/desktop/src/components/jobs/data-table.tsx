3ff85ba3ac2ce03318702ee8cf0e1592e604b78e
koraybey <21336342+koraybey@users.noreply.github.com>
2024-05-02 16:24:30 +0200

    add client side search filter

--- apps/desktop/src/components/jobs/data-table.tsx
+++ apps/desktop/src/components/jobs/data-table.tsx
@@ -1,9 +1,14 @@
 'use client'
 
-import type { ColumnDef, SortingState } from '@tanstack/react-table'
+import type {
+    ColumnDef,
+    ColumnFiltersState,
+    SortingState,
+} from '@tanstack/react-table'
 import {
     flexRender,
     getCoreRowModel,
+    getFilteredRowModel,
     getSortedRowModel,
     useReactTable,
 } from '@tanstack/react-table'
@@ -20,6 +25,8 @@ import {
     TableRow,
 } from '@/components/ui/table'
 
+import { Input } from '../ui/input'
+
 interface DataTableProperties<TData, TValue> {
     columns: ColumnDef<TData, TValue>[]
     data: TData[]
@@ -32,6 +39,7 @@ export const DataTable = <TData, TValue>({
     const setVacancy = useDetailStore((state) => state.setVacancy)
 
     const [sorting, setSorting] = useState<SortingState>([])
+    const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
 
     const table = useReactTable({
         data,
@@ -39,66 +47,90 @@ export const DataTable = <TData, TValue>({
         getCoreRowModel: getCoreRowModel(),
         onSortingChange: setSorting,
         getSortedRowModel: getSortedRowModel(),
+        onColumnFiltersChange: setColumnFilters,
+        getFilteredRowModel: getFilteredRowModel(),
         state: {
             sorting,
+            columnFilters,
         },
     })
 
     return (
-        <div className={'rounded-md border'}>
-            <Table>
-                <TableHeader>
-                    {table.getHeaderGroups().map((headerGroup) => (
-                        <TableRow key={headerGroup.id}>
-                            {headerGroup.headers.map((header) => {
-                                return (
-                                    <TableHead key={header.id}>
-                                        {header.isPlaceholder
-                                            ? undefined
-                                            : flexRender(
-                                                  header.column.columnDef
-                                                      .header,
-                                                  header.getContext()
-                                              )}
-                                    </TableHead>
-                                )
-                            })}
-                        </TableRow>
-                    ))}
-                </TableHeader>
-                <TableBody>
-                    {table.getRowModel().rows?.length ? (
-                        table.getRowModel().rows.map((row) => (
-                            <TableRow
-                                key={row.id}
-                                // eslint-disable-next-line react/jsx-no-bind
-                                onClick={() =>
-                                    setVacancy(row?.original as Vacancy)
-                                }
-                                data-state={row.getIsSelected() && 'selected'}
-                            >
-                                {row.getVisibleCells().map((cell) => (
-                                    <TableCell key={cell.id}>
-                                        {flexRender(
-                                            cell.column.columnDef.cell,
-                                            cell.getContext()
-                                        )}
-                                    </TableCell>
-                                ))}
+        <div className={'w-full'}>
+            <div className={'flex items-center pb-4'}>
+                <Input
+                    placeholder={'Search for a company...'}
+                    value={
+                        (table
+                            .getColumn('company')
+                            ?.getFilterValue() as string) ?? ''
+                    }
+                    // eslint-disable-next-line react/jsx-no-bind
+                    onChange={(event) =>
+                        table
+                            .getColumn('company')
+                            ?.setFilterValue(event.target.value)
+                    }
+                    className={'max-w-full'}
+                />
+            </div>
+            <div className={'rounded-md border'}>
+                <Table>
+                    <TableHeader>
+                        {table.getHeaderGroups().map((headerGroup) => (
+                            <TableRow key={headerGroup.id}>
+                                {headerGroup.headers.map((header) => {
+                                    return (
+                                        <TableHead key={header.id}>
+                                            {header.isPlaceholder
+                                                ? undefined
+                                                : flexRender(
+                                                      header.column.columnDef
+                                                          .header,
+                                                      header.getContext()
+                                                  )}
+                                        </TableHead>
+                                    )
+                                })}
                             </TableRow>
-                        ))
-                    ) : (
-                        <TableRow>
-                            <TableCell
-                                colSpan={columns.length}
-                                className={'h-24 text-center'}
-                            >
-                                No results.
-                            </TableCell>
-                        </TableRow>
-                    )}
-                </TableBody>
-            </Table>
+                        ))}
+                    </TableHeader>
+                    <TableBody>
+                        {table.getRowModel().rows?.length ? (
+                            table.getRowModel().rows.map((row) => (
+                                <TableRow
+                                    key={row.id}
+                                    // eslint-disable-next-line react/jsx-no-bind
+                                    onClick={() =>
+                                        setVacancy(row?.original as Vacancy)
+                                    }
+                                    data-state={
+                                        row.getIsSelected() && 'selected'
+                                    }
+                                >
+                                    {row.getVisibleCells().map((cell) => (
+                                        <TableCell key={cell.id}>
+                                            {flexRender(
+                                                cell.column.columnDef.cell,
+                                                cell.getContext()
+                                            )}
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                            ))
+                        ) : (
+                            <TableRow>
+                                <TableCell
+                                    colSpan={columns.length}
+                                    className={'h-24 text-center'}
+                                >
+                                    No results.
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </div>
         </div>
     )
 }
